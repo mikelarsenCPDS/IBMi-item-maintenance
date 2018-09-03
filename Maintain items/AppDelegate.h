66398415402818568a042c2ece0c2a432069c84b
Michael D Larsen <michaeldlarsen@michaels-macbook-air.local>
2018-09-03 16:19:07 -0400

    Initial Commit

---
+++ Maintain items/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Maintain items
+//
+//  Created by Michael D Larsen on 9/3/18.
+//  Copyright © 2018 Smash Alley, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Maintain items/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Maintain items
+//
+//  Created by Michael D Larsen on 9/3/18.
+//  Copyright © 2018 Smash Alley, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
